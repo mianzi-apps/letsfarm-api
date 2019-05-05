2a9f8b1625178f33168dc53a21af9fb77e4f18b9
byarustev <byarus45@gmail.com>
2019-05-05 18:28:27 +0300

    add crud

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -2,10 +2,10 @@ import express from 'express';
 import Question from '../controllers/questions';
 let router = express.Router();
 
-router.get('/questions',Question.listAllQuestions);
+router.get('/questions',Question.getAll);
 router.post('/questions',Question.create);
 router.put('/questions/:id',Question.update);
-router.get('/questions/:id',Question.getQuestion);
+router.get('/questions/:id',Question.getOne);
 router.delete('/questions/:id',Question.delete);
 
 module.exports = router;
