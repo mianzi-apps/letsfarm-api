aac2b4fe0e6c62d7dcdcd344c54e18f8ddc2ee6a
byarustev <byarus45@gmail.com>
2019-07-26 02:03:02 +0300

    update user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
-import UserModel from '../modelsp/user';
+import uuid from "uuid";
+const User = require('../models').User;
 import { generateToken } from '../middleware/authentication';
 
 const UserController={
@@ -8,33 +9,27 @@ const UserController={
      * @param res
      * @return {object} user
      */
-    async create(req, res){
-        // if (!req.body.display_name && !req.body.name) {
-        //     return res.status(400).send({'message': 'All fields are required'})
-        // }
-        const result = await UserModel.create(req.body);
-        if (result==='failure'){
+    create(req, res){
+        if (!req.body.display_name && !req.body.name) {
+            return res.status(400).send({'message': 'All fields are required'})
+        }
+        User.create({...req.body,id :uuid.v4()}).then((user)=>{
+            return res.status(201).send(user);
+        }).catch(()=>{
             return res.status(400).send({message:'operation failed'});
-        }else{
-            result.token = generateToken({'id':result.id});
-            return res.status(201).send(result);
-        }
-
+        })
     },
 
-    async login(req, res){
-        const result = await UserModel.login(req.body);
-        console.log(result);
-        if (result==='failure'){
+    login(req, res){
+        const {email,password} = req.body;
+        User.findOne({
+            where:{email,password}
+        }).then((user)=>{
+            user.token = generateToken({'id':user.id});
+            return res.status(201).send(user);
+        }).catch(()=>{
             return res.status(404).send({message:'invalid credentials'});
-        }else{
-            result.token = generateToken({'id':result.id});
-            return res.status(201).send(result);
-        }
-    },
-
-    mndjkFn(req, res) {
-        return res.status(200).send({success:'yes you did it'});
+        });
     },
 
 };
