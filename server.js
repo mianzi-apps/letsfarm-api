a5fe27b962aa0958ee9d4d186de1cd61725fe6c9
byarustev <byarus45@gmail.com>
2019-09-11 15:39:14 +0300

    allow authorization

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use( (request, response, next) => {
     response.header("Access-Control-Allow-Origin", "*");
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
     next();
 });
 app.use('/api/v1',questionRoutes);
